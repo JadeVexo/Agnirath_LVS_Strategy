aeba8da70b41f046832bcb3aaf2bd8b3b852c2cb
Jaay <jaayanth.jr@gmail.com>
2023-06-12 18:40:00 +0530

    Update 12/06/2023 6:40pm

--- map/coordinatesV3.py
+++ map/coordinatesV3.py
@@ -1,37 +1,3 @@
-# from flask_cors import CORS
-# from flask import Flask, jsonify
-# import random
-# import math
-
-# app = Flask(__name__)
-# CORS(app)
-
-# def generate_random_coordinates(center, radius, count):
-#     coordinates = []
-#     for _ in range(count):
-#         angle = random.uniform(0, 2 * math.pi)
-#         distance = random.uniform(0, radius)
-#         dx = distance * math.cos(angle)
-#         dy = distance * math.sin(angle)
-
-#         new_lng = center[0] + dx / 111320  # 111320 meters per degree longitude
-#         new_lat = center[1] + dy / 110540  # 110540 meters per degree latitude
-
-#         coordinates.append([new_lng, new_lat])
-
-#     return coordinates
-
-# @app.route('/get_random_coordinates')
-# def get_random_coordinates():
-#     center = [80.23021913617647, 12.993036661158213]
-#     radius = 1000  # 1 km
-#     count = 1
-#     random_coordinates = generate_random_coordinates(center, radius, count)
-#     return jsonify(random_coordinates)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask_cors import CORS
 from flask import Flask, jsonify
 import random

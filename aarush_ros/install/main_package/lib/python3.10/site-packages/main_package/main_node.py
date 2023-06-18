d3de206c43daeacd8eca1170a46b78d61fbc07c2
Jaay <jaayanth.jr@gmail.com>
2023-06-18 15:21:24 +0530

    Control Sim Update 18/06/2023 3:21pm

--- aarush_ros/install/main_package/lib/python3.10/site-packages/main_package/main_node.py
+++ aarush_ros/install/main_package/lib/python3.10/site-packages/main_package/main_node.py
@@ -1,5 +1,40 @@
-def main():
-    print('Hi from main_package.')
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray as rosarray
+
+
+class control_subscriber(Node):
+
+    def __init__(self):
+        super().__init__('control_node')
+        self.subscription = self.create_subscription(rosarray,'control_data',self.recieve_data,10)
+        self.subscription  # prevent unused variable warning
+        self.latest_data = None
+
+    def recieve_data(self, msg):
+        #self.get_logger().info(msg.data)
+        self.latest_data = msg.data
+    
+    def get_latest_data(self):
+        return self.latest_data
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    control_node = control_subscriber()
+
+    while rclpy.ok():
+        rclpy.spin_once(control_node)
+        latest_data = control_node.get_latest_data()
+        if latest_data is not None:
+            print(latest_data)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    control_node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':

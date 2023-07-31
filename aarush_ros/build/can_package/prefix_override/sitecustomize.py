74d36e61fcb4d57eff10f506d7338fd4c5f604a0
Ubuntu User <ubuntu@ubuntu.localdomain>
2023-07-31 13:48:01 +0000

    Updated can_package ROS file with DBC implementation 31/07/2023 8:30 PM

--- aarush_ros/build/can_package/prefix_override/sitecustomize.py
+++ aarush_ros/build/can_package/prefix_override/sitecustomize.py
@@ -1,7 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-<<<<<<< HEAD
-sys.prefix = sys.exec_prefix = '/home/veadesh/Agnirath/Agnirath_LVS_Strategy/aarush_ros/install/can_package'
-=======
 sys.prefix = sys.exec_prefix = '/home/ubuntu/Agnirath_LVS_Strategy/aarush_ros/install/can_package'
->>>>>>> origin/Aarush_ros

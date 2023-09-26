fa20093c135fbc2ebf7c096e634aa3c57a9fbb0e
Jaay <jaayanth.jr@gmail.com>
2023-09-26 14:12:49 +0530

    Update for Adruino

--- Arduino_Code/LoRa Sender/src/main.cpp
+++ Arduino_Code/LoRa Sender/src/main.cpp
@@ -1,32 +1,55 @@
 #include <Arduino.h>
-#include <SPI.h>
+#include <SPI.h> // include libraries
 #include <LoRa.h>
 
-int counter = 0;
+const int csPin = 10;   // LoRa radio chip select
+const int resetPin = 9; // LoRa radio reset
+const int irqPin = 2;   // change for your board; must be a hardware interrupt pin
+
+String outgoing; // outgoing message
+String concatenatedValues = "";
+
+byte msgCount = 0;        // count of outgoing messages
+byte localAddress = 0xBB; // address of this device
+byte destination = 0xBB;  // destination to send to
+long lastSendTime = 0;    // last send time
+int interval = 2000;      // interval between sends
+
+int txPower = 20;        // Transmit power in dBm
+int spreadingFactor = 7; // Spreading factor (SF7)
+int codingRate = 5;      // Coding rate (4/5)
+
+void sendMessage(String outgoing);
+void onReceive(int packetSize);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // initialize serial
   while (!Serial)
     ;
 
-  Serial.println("LoRa Sender");
+  Serial.println("LoRa Duplex");
+
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(csPin, resetPin, irqPin); // set CS, reset, IRQ pin
 
   if (!LoRa.begin(433E6))
-  {
-    Serial.println("Starting LoRa failed!");
-    while (1)
-      ;
+  { // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true)
+      ; // if failed, do nothing
   }
-  LoRa.setTxPower(20, 1);
+
+  LoRa.setTxPower(txPower, 1);
+  LoRa.setSpreadingFactor(spreadingFactor);
+  LoRa.setCodingRate4(codingRate);
+  Serial.println("LoRa init succeeded.");
 }
 
 void loop()
 {
-  if (Serial.available() > 0)
-  {
+  if (Serial.available() > 0){
     String receivedData = Serial.readStringUntil('\n');
-
     // Process the received comma-separated string of float values
     float receivedFloats[10]; // Adjust the array size as needed
     int floatCount = 0;
@@ -38,8 +61,7 @@ void loop()
       floatCount++;
       token = strtok(NULL, ",");
     }
-
-    // Concatenate the received float values into a single string
+    
     String concatenatedValues = "";
     for (int i = 0; i < floatCount; i++)
     {
@@ -49,15 +71,69 @@ void loop()
         concatenatedValues += ",";
       }
     }
-
-    // Send the concatenated string back to the computer
-    // Serial.println(concatenatedValues);
-    Serial.print("Sending packet: ");
-    Serial.println(concatenatedValues);
-    LoRa.beginPacket();
-    LoRa.print(concatenatedValues);
-    LoRa.endPacket();
-
-    counter++;
+    if (millis() - lastSendTime > interval)
+    {
+      String message = concatenatedValues; // send a message
+      sendMessage(message);
+      Serial.println("Sending " + message);
+      lastSendTime = millis(); // timestamp the message
+      interval = 1000;         // 2-3 seconds
+    }
+    // parse for a packet, and call onReceive with the result:
+    onReceive(LoRa.parsePacket());
   }
+}
+
+void sendMessage(String outgoing)
+{
+  LoRa.beginPacket();            // start packet
+  LoRa.write(destination);       // add destination address
+  LoRa.write(localAddress);      // add sender address
+  LoRa.write(msgCount);          // add message ID
+  LoRa.write(outgoing.length()); // add payload length
+  LoRa.print(outgoing);          // add payload
+  LoRa.endPacket();              // finish packet and send it
+  msgCount++;                    // increment message ID
+}
+
+void onReceive(int packetSize)
+{
+  if (packetSize == 0)
+    return; // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();       // recipient address
+  byte sender = LoRa.read();         // sender address
+  byte incomingMsgId = LoRa.read();  // incoming msg ID
+  byte incomingLength = LoRa.read(); // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available())
+  {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length())
+  { // check length for error
+    Serial.println("error: message length does not match length");
+    return; // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != destination)
+  {
+    Serial.println("This message is not for me.");
+    return; // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
 }
